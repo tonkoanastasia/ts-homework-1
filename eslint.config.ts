35b19634785b2515a9ce759dacb9523ff540a401
tonkoanastasia <tonkoanastasia@gmal.com>
2026-01-19 21:34:19 +0300

    feat: add eslint

---
+++ eslint.config.ts
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js, prettier },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  tseslint.configs.recommended,
+]);
